0f6c606e26776d870edba27849832e9a1e3526a5
Nitrionis <nitrodimon@mail.ru>
2019-12-19 17:15:50 +1000

    Add saber mouse controller support

--- Assets/Scripts/OculusRotationTest.cs
+++ Assets/Scripts/OculusRotationTest.cs
@@ -2,15 +2,21 @@
 
 public class OculusRotationTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-		
-    }
+#if UNITY_EDITOR
+	public float speed = 1;
+	private float x = 0;
+	private float y = 0;
 
-    // Update is called once per frame
-    void Update()
+#endif
+	// Update is called once per frame
+	void Update()
     {
+#if !UNITY_EDITOR
 		transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+#else
+		y += Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
+		x += Input.GetAxis("Mouse X") * Time.deltaTime * speed;
+		transform.rotation = Quaternion.Euler(new Vector3(y, x, 0));
+#endif
 	}
 }
